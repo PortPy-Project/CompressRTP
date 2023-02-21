8a65f8812a91cb9e2806ef4afc0eb359ccc1e5fb
Mojtaba Tefagh <mtefagh@ed.ac.uk>
2023-02-21 08:13:29 +0000

    compatibility with portpy-photon 0.0.4

--- low_dim_rt.py
+++ low_dim_rt.py
@@ -27,26 +27,16 @@ class LowDimRT:
         """
         num_of_beams = len(inf_matrix.beamlets_dict)
         low_dim_basis = dict.fromkeys(range(num_of_beams), [])
-        beamlets = dict()
-        beam_indices = list()
+        beamlets = inf_matrix.get_bev_2d_grid_in_orig_res(range(num_of_beams))
         index_position = list()
-        max_dim_1 = 0
-        max_dim_2 = 0
-        num_of_beamlets = 0
+        num_of_beamlets = inf_matrix.beamlets_dict[num_of_beams-1]['end_beamlet'] + 1
         for ind in range(num_of_beams):
-            beamlet = inf_matrix.create_beamlet_idx_2d_grid(ind)
-            beamlets[ind] = beamlet[range(0,len(beamlet),2)]
-            beam_indices.append(np.max(beamlets[ind])+1)
-            if beamlets[ind].shape[0] > max_dim_1:
-                max_dim_1 = beamlets[ind].shape[0]
-            if beamlets[ind].shape[1] > max_dim_2:
-                max_dim_2 = beamlets[ind].shape[1]
-            if beam_indices[ind] > num_of_beamlets:
-                num_of_beamlets = beam_indices[ind]
-            for i in range(0 if ind == 0 else beam_indices[ind-1], beam_indices[ind]):
+            for i in range(inf_matrix.beamlets_dict[ind]['start_beamlet'], inf_matrix.beamlets_dict[ind]['end_beamlet'] + 1):
                 index_position.append((np.where(beamlets[ind]==i)[0][0],np.where(beamlets[ind]==i)[1][0]))          
         if compression == 'wavelet':
-            beamlet_2d_grid = np.zeros((int(np.ceil(max_dim_1/2)), int(np.ceil(max_dim_2/2))))
+            max_dim_0 = np.max([beamlets[ind].shape[0] for ind in range(num_of_beams)])
+            max_dim_1 = np.max([beamlets[ind].shape[1] for ind in range(num_of_beams)])
+            beamlet_2d_grid = np.zeros((int(np.ceil(max_dim_0/2)), int(np.ceil(max_dim_1/2))))
             for row in range(beamlet_2d_grid.shape[0]):
                 for col in range(beamlet_2d_grid.shape[1]):
                     beamlet_2d_grid[row][col] = 1
@@ -56,7 +46,7 @@ class LowDimRT:
                         if 2*row-1 < beamlets[ind].shape[0] and 2*col-1 < beamlets[ind].shape[1] and beamlets[ind][2*row-1][2*col-1] != -1:
                             approximation = np.zeros(num_of_beamlets)
                             horizontal = np.zeros(num_of_beamlets)
-                            for i in range(0 if ind == 0 else beam_indices[ind-1], beam_indices[ind]):
+                            for i in range(inf_matrix.beamlets_dict[ind]['start_beamlet'], inf_matrix.beamlets_dict[ind]['end_beamlet'] + 1):
                                 approximation[i] = approximation_coeffs[index_position[i]]
                                 horizontal[i] = horizontal_coeffs[index_position[i]]
                             low_dim_basis[ind].append(np.concatenate((approximation, horizontal)))
