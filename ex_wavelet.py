a830beaea35b6acf46ec1a7bdbe2cddb56925337
gourav3017 <gourav.jhanwar007@gmail.com>
2023-03-19 12:44:03 -0400

    1. Updated tutorial.ipynb 2. Removed low dim rt optimization method because of new version of portpy==0.0.5

--- ex_wavelet.py
+++ ex_wavelet.py
@@ -5,6 +5,9 @@
 """
 import portpy_photon as pp
 from low_dim_rt import LowDimRT
+import numpy as np
+import cvxpy as cp
+import matplotlib.pyplot as plt
 
 
 def ex_wavelet():
@@ -16,27 +19,81 @@ def ex_wavelet():
     # create my_plan object for the planner beams_dict and select among the beams which are 30 degrees apart
     # for the customized beams_dict, you can pass the argument beam_ids
     my_plan = pp.Plan(patient_id, data_dir)
+
+    # Let us create rinds for creating reasonable dose fall off for the plan
+    rind_max_dose = np.array([1.1, 1.05, 0.9, 0.85, 0.75]) * my_plan.get_prescription()
+    rind_params = [{'rind_name': 'RIND_0', 'ref_structure': 'PTV', 'margin_start_mm': 0, 'margin_end_mm': 5,
+                    'max_dose_gy': rind_max_dose[0]},
+                   {'rind_name': 'RIND_1', 'ref_structure': 'PTV', 'margin_start_mm': 5, 'margin_end_mm': 10,
+                    'max_dose_gy': rind_max_dose[1]},
+                   {'rind_name': 'RIND_2', 'ref_structure': 'PTV', 'margin_start_mm': 10, 'margin_end_mm': 30,
+                    'max_dose_gy': rind_max_dose[2]},
+                   {'rind_name': 'RIND_3', 'ref_structure': 'PTV', 'margin_start_mm': 30, 'margin_end_mm': 60,
+                    'max_dose_gy': rind_max_dose[3]},
+                   {'rind_name': 'RIND_4', 'ref_structure': 'PTV', 'margin_start_mm': 60, 'margin_end_mm': 'inf',
+                    'max_dose_gy': rind_max_dose[4]}]
+    my_plan.add_rinds(rind_params=rind_params)
+
+    # create cvxpy problem using the clinical criteria
+    prob = pp.CvxPyProb(my_plan, opt_params={'smoothness_weight': 10})
+
+    # run imrt fluence map optimization using cvxpy and one of the supported solvers and save the optimal solution in sol
+    # CVXPy supports several opensource (ECOS, OSQP, SCS) and commercial solvers (e.g., MOSEK, GUROBI, CPLEX)
+    # For optimization problems with non-linear objective and/or constraints, MOSEK often performs well
+    # For mixed integer programs, GUROBI/CPLEX are good choices
+    # If you have .edu email address, you can get free academic license for commercial solvers
+    # we recommend the commercial solver MOSEK as your solver for the problems in this example,
+    # however, if you don't have a license, you can try opensource/free solver SCS or ECOS
+    # see https://www.cvxpy.org/tutorial/advanced/index.html for more info about CVXPy solvers
+    # To set up mosek solver, you can get mosek license file using edu account and place the license file in directory C:\Users\username\mosek
+    prob.solve(solver='MOSEK', verbose=True)
+    sol = prob.get_sol()
+
     # run IMRT fluence map optimization using a low dimensional subspace for fluence map compression
-    sol = pp.Optimize.run_IMRT_fluence_map_CVXPy(my_plan, solver='MOSEK', opt_params={'smoothness_weight': 10})
-    sol_low_dim = LowDimRT.run_IMRT_fluence_map_low_dim(my_plan, solver='MOSEK', opt_params={'smoothness_weight': 10})
-    sol_low_dim_only = LowDimRT.run_IMRT_fluence_map_low_dim(my_plan, solver='MOSEK',
-                                                             opt_params={'smoothness_weight': 0})
+    prob = pp.CvxPyProb(my_plan, opt_params={'smoothness_weight': 10})
+    # creating the wavelet incomplete basis representing a low dimensional subspace for dimension reduction
+    wavelet_basis = LowDimRT.get_low_dim_basis(my_plan.inf_matrix, 'wavelet')
+    # Smoothness Constraint
+    y = cp.Variable(wavelet_basis.shape[1])
+    prob.constraints += [wavelet_basis @ y == prob.x]
+    prob.solve(solver='MOSEK', verbose=False)
+    sol_low_dim = prob.get_sol()
+
+    # With no quadratic smoothness
+    prob = pp.CvxPyProb(my_plan, opt_params={'smoothness_weight': 0})
+    # creating the wavelet incomplete basis representing a low dimensional subspace for dimension reduction
+    wavelet_basis = LowDimRT.get_low_dim_basis(my_plan.inf_matrix, 'wavelet')
+    # Smoothness Constraint
+    y = cp.Variable(wavelet_basis.shape[1])
+    prob.constraints += [wavelet_basis @ y == prob.x]
+    prob.solve(solver='MOSEK', verbose=False)
+    sol_low_dim_only = prob.get_sol()
+
     # plot fluence 3D and 2D
-    pp.Visualize.plot_fluence_3d(sol=sol, beam_id=0)
-    pp.Visualize.plot_fluence_2d(sol=sol, beam_id=0)
-    pp.Visualize.plot_fluence_3d(sol=sol_low_dim, beam_id=0)
-    pp.Visualize.plot_fluence_2d(sol=sol_low_dim, beam_id=0)
-    pp.Visualize.plot_fluence_3d(sol=sol_low_dim_only, beam_id=0)
-    pp.Visualize.plot_fluence_2d(sol=sol_low_dim_only, beam_id=0)
+    fig, ax = plt.subplots(1, 3, figsize=(12, 12), subplot_kw={'projection': '3d'})
+    pp.Visualize.plot_fluence_3d(sol=sol, beam_id=0, ax=ax[0])
+    pp.Visualize.plot_fluence_3d(sol=sol_low_dim, beam_id=0, ax=ax[1])
+    pp.Visualize.plot_fluence_3d(sol=sol_low_dim_only, beam_id=0, ax=ax[2])
+    plt.show()
+
+    fig, ax = plt.subplots(1, 3, figsize=(12, 12))
+    pp.Visualize.plot_fluence_2d(sol=sol, beam_id=0, ax=ax[0])
+    pp.Visualize.plot_fluence_2d(sol=sol_low_dim, beam_id=0, ax=ax[1])
+    pp.Visualize.plot_fluence_2d(sol=sol_low_dim_only, beam_id=0, ax=ax[2])
+    plt.show()
     # plot DVH for the structures in the given list. Default dose_1d is in Gy and volume is in relative scale(%).
     structs = ['PTV', 'ESOPHAGUS', 'HEART', 'CORD']
-    my_plan.plot_dvh(sol=sol, structs=structs)
-    my_plan.plot_dvh(sol=sol_low_dim, structs=structs)
-    my_plan.plot_dvh(sol=sol_low_dim_only, structs=structs)
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax = pp.Visualize.plot_dvh(my_plan, sol=sol, structs=structs, ax=ax)
+    ax = pp.Visualize.plot_dvh(my_plan, sol=sol_low_dim, structs=structs, ax=ax)
+    pp.Visualize.plot_dvh(my_plan, sol=sol_low_dim_only, structs=structs, ax=ax)
+    plt.show()
     # plot 2d axial slice for the given solution and display the structures contours on the slice
-    pp.Visualize.plot_2d_dose(my_plan, sol=sol, slice_num=40, structs=['PTV'])
-    pp.Visualize.plot_2d_dose(my_plan, sol=sol_low_dim, slice_num=40, structs=['PTV'])
-    pp.Visualize.plot_2d_dose(my_plan, sol=sol_low_dim_only, slice_num=40, structs=['PTV'])
+    fig, ax = plt.subplots(1, 3, figsize=(12, 12))
+    pp.Visualize.plot_2d_dose(my_plan, sol=sol, slice_num=40, structs=['PTV'], ax=ax[0])
+    pp.Visualize.plot_2d_dose(my_plan, sol=sol_low_dim, slice_num=40, structs=['PTV'], ax=ax[1])
+    pp.Visualize.plot_2d_dose(my_plan, sol=sol_low_dim_only, slice_num=40, structs=['PTV'], ax=ax[2])
+    plt.show()
     # visualize plan metrics based upon clinical criteria
     pp.Visualize.plan_metrics(my_plan, sol=sol)
     pp.Visualize.plan_metrics(my_plan, sol=sol_low_dim)
--- low_dim_rt.py
+++ low_dim_rt.py
@@ -7,6 +7,7 @@ from portpy_photon.plan import Plan
 from portpy_photon.optimization import Optimization
 from portpy_photon.influence_matrix import InfluenceMatrix
 
+
 class LowDimRT:
     """
     An incomplete basis representing a low dimensional subspace for fluence map compression
@@ -19,7 +20,7 @@ class LowDimRT:
             Optimization method for creating and optimizing the IMRT treatment plan
 
     """
-    
+
     @staticmethod
     def get_low_dim_basis(inf_matrix: InfluenceMatrix, compression: str = 'wavelet'):
         """
@@ -31,171 +32,41 @@ class LowDimRT:
         num_of_beams = len(inf_matrix.beamlets_dict)
         low_dim_basis = list()
         beam_id = [inf_matrix.beamlets_dict[i]['beam_id'] for i in range(num_of_beams)]
-        beamlets = inf_matrix.get_bev_2d_grid_in_orig_res(beam_id=beam_id)
+        beamlets = inf_matrix.get_bev_2d_grid(beam_id=beam_id)
         index_position = list()
-        num_of_beamlets = inf_matrix.beamlets_dict[num_of_beams-1]['end_beamlet'] + 1
+        num_of_beamlets = inf_matrix.beamlets_dict[num_of_beams - 1]['end_beamlet'] + 1
         for ind in range(num_of_beams):
-            for i in range(inf_matrix.beamlets_dict[ind]['start_beamlet'], inf_matrix.beamlets_dict[ind]['end_beamlet'] + 1):
-                index_position.append((np.where(beamlets[ind]==i)[0][0],np.where(beamlets[ind]==i)[1][0]))          
+            for i in range(inf_matrix.beamlets_dict[ind]['start_beamlet'],
+                           inf_matrix.beamlets_dict[ind]['end_beamlet'] + 1):
+                index_position.append((np.where(beamlets[ind] == i)[0][0], np.where(beamlets[ind] == i)[1][0]))
         if compression == 'wavelet':
             max_dim_0 = np.max([beamlets[ind].shape[0] for ind in range(num_of_beams)])
             max_dim_1 = np.max([beamlets[ind].shape[1] for ind in range(num_of_beams)])
-            beamlet_2d_grid = np.zeros((int(np.ceil(max_dim_0/2)), int(np.ceil(max_dim_1/2))))
+            beamlet_2d_grid = np.zeros((int(np.ceil(max_dim_0 / 2)), int(np.ceil(max_dim_1 / 2))))
             for row in range(beamlet_2d_grid.shape[0]):
                 for col in range(beamlet_2d_grid.shape[1]):
                     beamlet_2d_grid[row][col] = 1
-                    approximation_coeffs = pywt.idwt2((beamlet_2d_grid, (None, None, None)), 'sym4', mode='periodization')
+                    approximation_coeffs = pywt.idwt2((beamlet_2d_grid, (None, None, None)), 'sym4',
+                                                      mode='periodization')
                     horizontal_coeffs = pywt.idwt2((None, (beamlet_2d_grid, None, None)), 'sym4', mode='periodization')
                     for b in range(num_of_beams):
-                        if ((2*row+1<beamlets[b].shape[0] and 2*col+1<beamlets[b].shape[1] and beamlets[b][2*row+1][2*col+1] != -1) or 
-                            (2*row+1<beamlets[b].shape[0] and 2*col<beamlets[b].shape[1] and beamlets[b][2*row+1][2*col] != -1) or 
-                            (2*row<beamlets[b].shape[0] and 2*col+1<beamlets[b].shape[1] and beamlets[b][2*row][2*col+1] != -1) or
-                            (2*row<beamlets[b].shape[0] and 2*col<beamlets[b].shape[1] and beamlets[b][2*row][2*col] != -1)):
+                        if ((2 * row + 1 < beamlets[b].shape[0] and 2 * col + 1 < beamlets[b].shape[1] and
+                             beamlets[b][2 * row + 1][2 * col + 1] != -1) or
+                                (2 * row + 1 < beamlets[b].shape[0] and 2 * col < beamlets[b].shape[1] and
+                                 beamlets[b][2 * row + 1][2 * col] != -1) or
+                                (2 * row < beamlets[b].shape[0] and 2 * col + 1 < beamlets[b].shape[1] and
+                                 beamlets[b][2 * row][2 * col + 1] != -1) or
+                                (2 * row < beamlets[b].shape[0] and 2 * col < beamlets[b].shape[1] and
+                                 beamlets[b][2 * row][2 * col] != -1)):
                             approximation = np.zeros(num_of_beamlets)
                             horizontal = np.zeros(num_of_beamlets)
-                            for ind in range(inf_matrix.beamlets_dict[b]['start_beamlet'], inf_matrix.beamlets_dict[b]['end_beamlet'] + 1):
+                            for ind in range(inf_matrix.beamlets_dict[b]['start_beamlet'],
+                                             inf_matrix.beamlets_dict[b]['end_beamlet'] + 1):
                                 approximation[ind] = approximation_coeffs[index_position[ind]]
                                 horizontal[ind] = horizontal_coeffs[index_position[ind]]
                             low_dim_basis.append(np.stack((approximation, horizontal)))
                     beamlet_2d_grid[row][col] = 0
         low_dim_basis = np.transpose(np.concatenate(low_dim_basis, axis=0))
-        u, s, vh = scipy.sparse.linalg.svds(low_dim_basis, k = min(low_dim_basis.shape[0], low_dim_basis.shape[1]) - 1)
+        u, s, vh = scipy.sparse.linalg.svds(low_dim_basis, k=min(low_dim_basis.shape[0], low_dim_basis.shape[1]) - 1)
         ind = np.where(s > 0.0001)
         return u[:, ind[0]]
-    
-    @staticmethod
-    def run_IMRT_fluence_map_low_dim(my_plan: Plan, inf_matrix: InfluenceMatrix = None, solver: str = 'MOSEK',
-                                   verbose: bool = True, cvxpy_options: dict = None, **opt_params) -> dict:
-        """
-        It runs optimization to create optimal plan based upon clinical criteria
-        :param my_plan: object of class Plan
-        :param inf_matrix: object of class InfluenceMatrix
-        :param solver: default solver 'MOSEK'. check cvxpy website for available solvers
-        :param verbose: Default to True. If set to False, it will not print the solver iterations.
-        :param cvxpy_options: cvxpy and the solver settings
-        :param opt_params: optimization parameters for modifying parameters of problem statement
-        :return: returns the solution dictionary in format of:
-            dict: {
-                   'optimal_intensity': list(float),
-                   'dose_1d': list(float),
-                   'inf_matrix': Pointer to object of InfluenceMatrix }
-                  }
-        :Example:
-        >>> Optimization.run_IMRT_fluence_map_CVXPy(my_plan=my_plan,inf_matrix=inf_matrix,solver='MOSEK')
-        """
-
-        if cvxpy_options is None:
-            cvxpy_options = dict()
-
-        t = time.time()
-
-        # get data for optimization
-        if inf_matrix is None:
-            inf_matrix = my_plan.inf_matrix
-        A = inf_matrix.A
-        cc_dict = my_plan.clinical_criteria.clinical_criteria_dict
-        criteria = cc_dict['criteria']
-        pres = cc_dict['pres_per_fraction_gy']
-        num_fractions = cc_dict['num_of_fractions']
-        [Qx, Qy, num_rows, num_cols] = Optimization.get_smoothness_matrix(inf_matrix.beamlets_dict)
-        st = inf_matrix
-
-        # create and add rind constraints
-        rinds = ['RIND_0', 'RIND_1', 'RIND_2', 'RIND_3', 'RIND_4']
-        if rinds[0] not in my_plan.structures.structures_dict[
-            'name']:  # check if rind is already created. If yes, skip rind creation
-            Optimization.create_rinds(my_plan, size_mm=[5, 5, 20, 30, 500])
-            Optimization.set_rinds_opt_voxel_idx(my_plan,
-                                                 inf_matrix=inf_matrix)  # rind_0 is 5mm after PTV, rind_2 is 5 mm after rind_1, and so on..
-        else:
-            Optimization.set_rinds_opt_voxel_idx(my_plan, inf_matrix=inf_matrix)
-
-        rind_max_dose_perc = [1.1, 1.05, 0.9, 0.85, 0.75]
-        for i, rind in enumerate(rinds):  # Add rind constraints
-            parameters = {'structure_name': rind}
-            total_pres = cc_dict['pres_per_fraction_gy'] * cc_dict['num_of_fractions']
-            constraints = {'limit_dose_gy': total_pres * rind_max_dose_perc[i]}
-            my_plan.clinical_criteria.add_criterion(criterion='max_dose', parameters=parameters,
-                                                    constraints=constraints)
-
-        # # setting weights for oar objectives
-        all_vox = np.arange(A.shape[0])
-        oar_voxels = all_vox[~np.isin(np.arange(A.shape[0]), st.get_opt_voxels_idx('PTV'))]
-        oar_weights = np.ones(A[oar_voxels, :].shape[0])
-        if cc_dict['disease_site'] == 'Prostate':
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('RECT_WALL')))] = 20
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('BLAD_WALL')))] = 5
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('RIND_0')))] = 3
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('RIND_1')))] = 3
-        elif cc_dict['disease_site'] == 'Lung':
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('CORD')))] = 10
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('ESOPHAGUS')))] = 20
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('HEART')))] = 20
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('RIND_0')))] = 3
-            oar_weights[np.where(np.isin(oar_voxels, st.get_opt_voxels_idx('RIND_1')))] = 3
-
-        # Construct the problem.
-        x = cp.Variable(A.shape[1], pos=True)
-        dO = cp.Variable(len(st.get_opt_voxels_idx('PTV')), pos=True)
-        dU = cp.Variable(len(st.get_opt_voxels_idx('PTV')), pos=True)
-
-        # Form objective.
-        ptv_overdose_weight = opt_params['ptv_overdose_weight'] if 'ptv_overdose_weight' in opt_params else 10000
-        ptv_underdose_weight = opt_params['ptv_underdose_weight'] if 'ptv_underdose_weight' in opt_params else 100000
-        smoothness_weight = opt_params['smoothness_weight'] if 'smoothness_weight' in opt_params else 10
-        total_oar_weight = opt_params['total_oar_weight'] if 'total_oar_weight' in opt_params else 10
-        smoothness_X_weight = 0.6
-        smoothness_Y_weight = 0.4
-
-        print('Objective Start')
-        obj = [(1 / len(st.get_opt_voxels_idx('PTV'))) * (
-                ptv_overdose_weight * cp.sum_squares(dO) + ptv_underdose_weight * cp.sum_squares(dU)),
-               smoothness_weight * (
-                       smoothness_X_weight * (1/num_cols) * cp.sum_squares(Qx @ x) + smoothness_Y_weight * (1/num_rows) * cp.sum_squares(Qy @ x)),
-               total_oar_weight * (1 / A[oar_voxels, :].shape[0]) * cp.sum_squares(
-                   cp.multiply(cp.sqrt(oar_weights), A[oar_voxels, :] @ x))]
-
-        print('Objective done')
-        print('Constraints Start')
-        constraints = []
-        # constraints += [wMean == cp.sum(w)/w.shape[0]]
-        for i in range(len(criteria)):
-            if 'max_dose' in criteria[i]['name']:
-                if 'limit_dose_gy' in criteria[i]['constraints']:
-                    limit = criteria[i]['constraints']['limit_dose_gy']
-                    org = criteria[i]['parameters']['structure_name']
-                    if org != 'GTV' or org != 'CTV':
-                        constraints += [A[st.get_opt_voxels_idx(org), :] @ x <= limit / num_fractions]
-            elif 'mean_dose' in criteria[i]['name']:
-                if 'limit_dose_gy' in criteria[i]['constraints']:
-                    limit = criteria[i]['constraints']['limit_dose_gy']
-                    org = criteria[i]['parameters']['structure_name']
-                    # mean constraints using voxel weights
-                    constraints += [(1 / sum(st.get_opt_voxels_size(org))) *
-                                    (cp.sum((cp.multiply(st.get_opt_voxels_size(org), A[st.get_opt_voxels_idx(org),
-                                                                                      :] @ x)))) <= limit / num_fractions]
-
-        # Step 1 and 2 constraint
-        constraints += [A[st.get_opt_voxels_idx('PTV'), :] @ x <= pres + dO]
-        constraints += [A[st.get_opt_voxels_idx('PTV'), :] @ x >= pres - dU]
-
-        # creating the wavelet incomplete basis representing a low dimensional subspace for dimension reduction
-        wavelet_basis = LowDimRT.get_low_dim_basis(inf_matrix, 'wavelet')
-        # Smoothness Constraint
-        y = cp.Variable(wavelet_basis.shape[1])
-        constraints += [wavelet_basis @ y == x]
-
-        print('Constraints Done')
-
-        prob = cp.Problem(cp.Minimize(sum(obj)), constraints)
-
-        print('Problem loaded')
-        prob.solve(solver=solver, verbose=verbose, **cvxpy_options)
-        print("optimal value with {}:{}".format(solver, prob.value))
-        elapsed = time.time() - t
-        print('Elapsed time {} seconds'.format(elapsed))
-
-        # saving optimal solution to the solution dictionary
-        sol = {'optimal_intensity': x.value.astype('float32'), 'inf_matrix': inf_matrix}
-
-        return sol
